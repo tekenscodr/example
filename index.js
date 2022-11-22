0e63c44fa5e9d52d77e47b65b5d9917bb16ca3af
tekenscodr <ensleybdarku@gmail.com>
2022-11-22 19:32:54 +0000

    jar

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res)=>{
-  res.json("You are in the server");
+  res.send("You are in the server");
 })
 
 const PORT = 3000
